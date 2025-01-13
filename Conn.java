1ebd465cbd6e925d7b69e893b91a58bf1bf5d9e9
tinytanya01 <161453248+tinytanya01@users.noreply.github.com>
2025-01-13 17:59:24 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,19 @@
+package airlinemanagement;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    public Conn() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///airlinemanagementsystem", "root", "Anisha@01");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
